e9648a213a2e52c7ba5d42e817ba2d2c35043e9a
virtualclone <q199493422@gmail.com>
2018-05-25 14:06:14 +0800

    update

--- src/compute_imu_bias.cpp
+++ src/compute_imu_bias.cpp
@@ -1,6 +1,6 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
-
+#include <eigen3/Eigen/Dense>
 sensor_msgs::Imu imu_data;
 
 void imu_cb(const sensor_msgs::Imu::ConstPtr &msg){
@@ -17,18 +17,22 @@ int main(int argc, char **argv)
 
   ros::Rate rate(50);
   float sum_x = 0,sum_y = 0, sum_z = 0, avg_x = 0, avg_y = 0, avg_z = 0;
-  int count = 0;
+  int count = 0, i = 0;
+  Eigen::VectorXd linear_x;
   while(ros::ok()){
-    /*
+
   if (imu_data.linear_acceleration.x !=0 && imu_data.linear_acceleration.y !=0 ){
-  sum_x += imu_data.linear_acceleration.x;
-  sum_y += imu_data.linear_acceleration.y;
-  sum_z += imu_data.linear_acceleration.z;
-  count += 1;
-  avg_x = sum_x / count;
-  avg_y = sum_y / count;
-  avg_z = sum_z / count;
-}
+    linear_x(i) = imu_data.linear_acceleration.x;
+    i += 1;
+    sum_x += imu_data.linear_acceleration.x;
+    sum_y += imu_data.linear_acceleration.y;
+    sum_z += imu_data.linear_acceleration.z;
+    count += 1;
+    avg_x = sum_x / count;
+    avg_y = sum_y / count;
+    avg_z = sum_z / count;
+  }
+
   ROS_INFO("Bias: ax = %f, ay = %f, az = %f", avg_x, avg_y, avg_z);
 */
     if(abs(imu_data.linear_acceleration.x) > 1 ){
